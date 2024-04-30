92435f579b82d51d7150363c267ef1f35914cb85
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2024-04-30 14:16:32 -0700

    connection status by useTracker

--- app/src/hooks/useConnection.js
+++ app/src/hooks/useConnection.js
@@ -1,43 +1,52 @@
 import { useEffect, useState } from 'react'
-import Meteor from '@meteorrn/core'
+import Meteor, { useTracker } from '@meteorrn/core'
 import * as SecureStore from 'expo-secure-store'
 import config from '../../config.json'
 
+// get detailed info about internals
 Meteor.isVerbose = true
 
+// connect with Meteor and use a secure store
+// to persist our received login token, so it's encrypted
+// and only readable for this very app
+// read more at: https://docs.expo.dev/versions/latest/sdk/securestore/
 Meteor.connect(config.backend.url, {
   AsyncStorage: {
     getItem: SecureStore.getItemAsync,
     setItem: SecureStore.setItemAsync,
     removeItem: SecureStore.deleteItemAsync,
-  }
+  },
+  autoConnect: true,
+  autoReconnect: true,
+  reconnectInterval: 500,
 })
 
+/**
+ * Hook that handle auto-reconnect and updates state accordingly.
+ * @return {{connected: boolean|null, connectionError: Error|null}}
+ */
 export const useConnection = () => {
   const [connected, setConnected] = useState(null)
   const [connectionError, setConnectionError] = useState(null)
+  const status = useTracker(() => Meteor.status())
 
+  if (status.connected && !connected) {
+    console.info('set connected', status)
+    setConnected(true)
+  }
+
+  if (connected && !status.connected) {
+    console.info('set disconnected', status)
+    setConnected(false)
+  }
+
+  // we additionally listen to any connection errors
   useEffect(() => {
     const onError = (e) => setConnectionError(e)
     Meteor.ddp.on('error', onError)
 
-    const onConnected = () => connected !== true && setConnected(true)
-    Meteor.ddp.on('connected', onConnected)
-    
-    const onDisconnected = () => {
-      Meteor.ddp.autoConnect = true
-      if (connected !== false) {
-        setConnected(false)
-      }
-      Meteor.reconnect()
-    }
-
-    Meteor.ddp.on('disconnected', onDisconnected)
-
     return () => {
       Meteor.ddp.off('error', onError)
-      Meteor.ddp.off('connected', onConnected)
-      Meteor.ddp.off('disconnected', onDisconnected)
     }
   }, [])
   
